ed94e105a087023c17a078a83277afef5c7f9fc4
Akagami no Shanks <hermawanriszky14@mgmail.com>
2022-05-30 13:07:24 +0700

    styling shop page

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,8 +1,6 @@
-import { Button } from "react-bootstrap"
-import Card from 'react-bootstrap/Card'
-import CardGroup from 'react-bootstrap/CardGroup'
-import { Container } from "react-bootstrap"
-import logo from '../assets/img/LiquizLogo.png'
+import { Button, Container, Card, CardGroup, Col, Row, Image } from "react-bootstrap"
+import coins from '../assets/img/coins.png'
+import '../style/Shop.css'
 
 //Ini gambar buat ngetes doang, nanti ganti aja
 function Shop() {
@@ -34,10 +32,99 @@ function Shop() {
     ];
     return (
         <Container>
-            <div className="shop">
-                <CardGroup>
-                    <Card>
-                        <Card.Img variant="top" src={logo} />
+            <br></br>
+            <Row>
+                <Col>
+                    <div className="coinCard">
+                        {shopList[0].itemName}
+                        <br></br>
+                        <Image
+                            src={coins}
+                            height="110"
+                            className="rounded coinimg"
+                            alt="Liquiz Logo"
+                        />
+                        <Button className="coinButton">{shopList[0].price}</Button>
+                    </div>
+                </Col>
+                <Col>
+                    <div className="coinCard">
+                        {shopList[1].itemName}
+                        <br></br>
+                        <Image
+                            src={coins}
+                            height="110"
+                            className="rounded coinimg"
+                            alt="Liquiz Logo"
+                        />
+                        <Button className="coinButton">{shopList[1].price}</Button>
+                    </div>
+                </Col>
+                <Col>
+                    <div className="coinCard">
+                        {shopList[2].itemName}
+                        <br></br>
+                        <Image
+                            src={coins}
+                            height="110"
+                            className="rounded coinimg"
+                            alt="Liquiz Logo"
+                        />
+                        <Button className="coinButton">{shopList[2].price}</Button>
+                    </div>
+                </Col>
+            </Row>
+            <br></br>
+            <Row className="mt-30">
+                <Col>
+                    <div className="coinCard">
+                        {shopList[3].itemName}
+                        <br></br>
+                        <Image
+                            src={coins}
+                            height="110"
+                            className="rounded coinimg"
+                            alt="Liquiz Logo"
+                        />
+                        <Button className="coinButton">{shopList[3].price}</Button>
+                    </div>
+                </Col>
+                <Col>
+                    <div className="coinCard">
+                        {shopList[4].itemName}
+                        <br></br>
+                        <Image
+                            src={coins}
+                            height="110"
+                            className="rounded coinimg"
+                            alt="Liquiz Logo"
+                        />
+                        <Button className="coinButton">{shopList[4].price}</Button>
+                    </div>
+                </Col>
+                <Col>
+                    <div className="coinCard">
+                        {shopList[5].itemName}
+                        <br></br>
+                        <Image
+                            src={coins}
+                            height="110"
+                            className="rounded coinimg"
+                            alt="Liquiz Logo"
+                        />
+                        <Button className="coinButton">{shopList[5].price}</Button>
+                    </div>
+                </Col>
+            </Row>
+            {/* <div className="shop">
+                <CardGroup className="m-10">
+                    <Card className="shopCard">
+                        <Image
+                            src={coins}
+                            height="228"
+                            className="rounded loginLogo"
+                            alt="coins"
+                        />
                         <Card.Body>
                             <Card.Title>{shopList[0].itemName}</Card.Title>
                             <Card.Text>
@@ -48,8 +135,8 @@ function Shop() {
                             <Button variant="dark">{shopList[0].price}</Button>
                         </Card.Footer>
                     </Card>
-                    <Card>
-                        <Card.Img variant="top" src={logo} />
+                    <Card className="shopCard">
+                        <Card.Img variant="top" src={coins} />
                         <Card.Body>
                             <Card.Title>{shopList[1].itemName}</Card.Title>
                             <Card.Text>
@@ -60,8 +147,8 @@ function Shop() {
                             <Button variant="dark">{shopList[1].price}</Button>
                         </Card.Footer>
                     </Card>
-                    <Card>
-                        <Card.Img variant="top" src={logo} />
+                    <Card className="shopCard">
+                        <Card.Img variant="top" src={coins} />
                         <Card.Body>
                             <Card.Title>{shopList[2].itemName}</Card.Title>
                             <Card.Text>
@@ -74,8 +161,8 @@ function Shop() {
                     </Card>
                 </CardGroup>
                 <CardGroup>
-                    <Card>
-                        <Card.Img variant="top" src={logo} />
+                    <Card className="shopCard">
+                        <Card.Img variant="top" src={coins} />
                         <Card.Body>
                             <Card.Title>{shopList[3].itemName}</Card.Title>
                             <Card.Text>
@@ -86,8 +173,8 @@ function Shop() {
                             <Button variant="dark">{shopList[3].price}</Button>
                         </Card.Footer>
                     </Card>
-                    <Card>
-                        <Card.Img variant="top" src={logo} />
+                    <Card className="shopCard">
+                        <Card.Img variant="top" src={coins} />
                         <Card.Body>
                             <Card.Title>{shopList[4].itemName}</Card.Title>
                             <Card.Text>
@@ -98,8 +185,8 @@ function Shop() {
                             <Button variant="dark">{shopList[4].price}</Button>
                         </Card.Footer>
                     </Card>
-                    <Card>
-                        <Card.Img variant="top" src={logo} />
+                    <Card className="shopCard">
+                        <Card.Img variant="top" src={coins} />
                         <Card.Body>
                             <Card.Title>{shopList[5].itemName}</Card.Title>
                             <Card.Text>
@@ -111,7 +198,7 @@ function Shop() {
                         </Card.Footer>
                     </Card>
                 </CardGroup>
-            </div>
+            </div> */}
         </Container>
     )
 }
