051154c71afc6e4431c52e8123467afb295dc9d6
00shiki <ahmadluhurp@gmail.com>
2022-05-31 02:48:47 +0700

    make login logout components in the navbar

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,56 +1,36 @@
-import { Navbar, Container, Nav } from "react-bootstrap"
+import { Container, Nav } from "react-bootstrap"
+import { useState, useEffect } from 'react'
 import logo from '../assets/img/LiquizLogo.png'
 import Image from 'react-bootstrap/Image'
+import NavLog from './NavLog.js'
 
 const NavigationBar = () => {
-  function Logout() {
-    localStorage.clear()
-    window.location.href = "/login"
-  }
-  const Token = localStorage.getItem('token')
-  if (Token == null) {
-    return (
-      <Container>
-        <Nav className="NavigationBar justify-content-center ">
-          <Nav.Link className="text-center text-white navlink" href="/">Home</Nav.Link>
-          <Nav.Link className="text-center text-white navlink" href="/leaderboard">Leaderboard</Nav.Link>
-          <Nav.Link>
-            <Image
-              src={logo}
-              height="37"
-              className="d-inline-block align-top rounded mx-auto d-block Navlogo"
-              alt="Liquiz Logo"
-            />
-          </Nav.Link>
-          <Nav.Link className="text-center text-white navlink" href="/shop">Coin Shop</Nav.Link>
-          <Nav.Link className="text-center text-white navlink" href="/login">Login/Register</Nav.Link>
-          <Nav.Link className="text-center text-white navlink" onClick={Logout}>Logout</Nav.Link>
-        </Nav>
-      </Container>
-    )
-  }
-  else {
-    return (
-      <Container>
-        <Nav className="NavigationBar justify-content-center ">
-          <Nav.Link className="text-center text-white navlink" href="/">Home</Nav.Link>
-          <Nav.Link className="text-center text-white navlink" href="/leaderboard">Leaderboard</Nav.Link>
-          <Nav.Link>
-            <Image
-              src={logo}
-              height="37"
-              className="d-inline-block align-top rounded mx-auto d-block Navlogo"
-              alt="Liquiz Logo"
-            />
-          </Nav.Link>
-          <Nav.Link className="text-center text-white navlink" href="/shop">Coin Shop</Nav.Link>
-          <Nav.Link className="text-center text-white navlink" href="/profile">Profil</Nav.Link>
-          <Nav.Link className="text-center text-white navlink" onClick={Logout}>Logout</Nav.Link>
-        </Nav>
-      </Container>
-    )
-  }
+  const token = localStorage.getItem('token');
+  const [logged, setLogged] = useState('logged');
 
+  useEffect(() => {
+    if (token === null) {
+      setLogged('')
+    }
+  }, [token, logged]);
+  
+  return (
+    <Container>
+      <Nav className="NavigationBar justify-content-center ">
+        <Nav.Link className="text-center text-white navlink" href="/">Home</Nav.Link>
+        <Nav.Link className="text-center text-white navlink" href="/leaderboard">Leaderboard</Nav.Link>
+        <Nav.Link>
+          <Image
+            src={logo}
+            height="37"
+            className="d-inline-block align-top rounded mx-auto d-block Navlogo"
+            alt="Liquiz Logo"
+          />
+        </Nav.Link>
+        <Nav.Link className="text-center text-white navlink" href="/shop">Coin Shop</Nav.Link>
+        <NavLog log={logged} />
+      </Nav>
+    </Container>
+  )
 }
-
 export default NavigationBar
