25a5b1d49c410c7d4ee3b16c1085d6dff8165ad5
Akagami no Shanks <hermawanriszky14@mgmail.com>
2022-05-30 10:12:27 +0700

    Styling Login and Register Page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,55 +1,87 @@
-import { Form,Button } from 'react-bootstrap'
-import React ,{useState} from 'react'
+import { Form, Button, Container, Row, Col, Image } from 'react-bootstrap'
+import React, { useState } from 'react'
 import axios, { Axios } from 'axios'
+import '../style/login.css'
+import Card from 'react-bootstrap/Card'
+import logo from '../assets/img/LiquizLogo.png'
+import loginImage from '../assets/img/login.png'
 
 function Login() {
-    const [user, setUser] = useState({});
-    
-     const handleChange = (e) => {
-        const newUser = {...user};
-        newUser[e.target.name] = e.target.value;
-        setUser(newUser);
-      }; 
-    
-      const handleSubmit = (e) => {
-        e.preventDefault();
-        const userData = {
-          username: user.username,
-          password: user.password
-        };
-        axios.post("https://liquiz-backend.herokuapp.com/api/login", userData)
-        .then((response) => {
-          console.log(response.status);
-          console.log(response.data.token);
-        })
-        .catch((error) => {
-            if (error.response) {
-              console.log(error.response);
-              console.log("server responded");
-            } else if (error.request) {
-              console.log("network error");
-            } else {
-              console.log(error);
-            }
-        });
-      };
+  const [user, setUser] = useState({});
 
-    return (
-        <div className='form'>
-            <h1>MASUK</h1>
-            <Form onSubmit={handleSubmit}>
+  const handleChange = (e) => {
+    const newUser = { ...user };
+    newUser[e.target.name] = e.target.value;
+    setUser(newUser);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const userData = {
+      username: user.username,
+      password: user.password
+    };
+    axios.post("https://liquiz-backend.herokuapp.com/api/login", userData)
+      .then((response) => {
+        console.log(response.status);
+        console.log(response.data.token);
+      })
+      .catch((error) => {
+        if (error.response) {
+          console.log(error.response);
+          console.log("server responded");
+        } else if (error.request) {
+          console.log("network error");
+        } else {
+          console.log(error);
+        }
+      });
+  };
+
+  return (
+    <Container>
+      <Card className="loginCard">
+        <Row>
+          <Col className="justify-content-left">
+            <div className='form'>
+              <Image
+              src={logo}
+              height="110"
+              className="rounded loginLogo"
+              alt="Liquiz Logo"
+              />
+              <br></br>
+              <h1 className="welcomeText">Welcome Back !!!</h1>
+              <br></br>
+              <br></br>
+              <Form onSubmit={handleSubmit}>
                 <Form.Group className="mb-3" onChange={handleChange} value={user.username}>
-                    <Form.Control name="username" type="text" placeholder="Masukkan Username" />
+                  <Form.Label className="labelLogin bsPrefix">Username</Form.Label>
+                  <Form.Control name="username" type="text" className="formLogin" />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword" onChange={handleChange} value={user.password}>
-                    <Form.Control name="password" type="password" placeholder="Masukkan Password" />
+                  <Form.Label className="labelLogin bsPrefix">Password</Form.Label>
+                  <Form.Control name="password" type="password"  className="formLogin" />
                 </Form.Group>
-                <div className='option'>Belum punya akun? Daftar <a href='/register'>disini.</a></div>
-                <Button variant="primary" type="submit" onClick={Login}>Masuk</Button>
-            </Form>
-        </div>
-    )
+                <div className='option ml-10'>Belum punya akun? Daftar <a href='/register'>disini.</a></div>
+                <br></br>
+                <Button variant="primary" type="submit" onClick={Login} className="loginButton">LOGIN</Button>
+              </Form>
+            </div>
+          </Col>
+          <Col>
+          <Image
+              src={loginImage}
+              height="800"
+              className="rounded mt-100"
+              alt="Login Image"
+              />
+          </Col>
+        </Row>
+      </Card>
+    </Container>
+  )
 }
 
 export default Login
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,6 +1,9 @@
-import { Form,Button } from 'react-bootstrap'
+import { Form,Button, Container, Row, Col, Card, Image } from 'react-bootstrap'
 import React ,{useState} from 'react'
 import axios from 'axios'
+import logo from '../assets/img/LiquizLogo.png'
+import '../style/regis.css'
+import regisImage from '../assets/img/register.png'
 
 function Register() {
     const [user, setUser] = useState({});
@@ -25,24 +28,55 @@ function Register() {
       };
 
     return (
-        <div className='form'>
-            <h1>DAFTAR</h1>
-            <Form onSubmit={handleSubmit}>
-                <Form.Group className="mb-3" value={user.username}
-                onChange={handleChange}>
-                    <Form.Control name="username" type="text" placeholder="Masukkan Username" />
-                </Form.Group>
+      <Container>
+      <Card className="loginCard">
+        <Row>
+          <Col className="justify-content-left">
+          <Image
+              src={regisImage}
+              height="750"
+              className="rounded mt-100"
+              alt="Regis Image"
+              />
+          </Col>
+          <Col className="justify-content-left">
+            <div className='form'>
+              <Image
+                src={logo}
+                height="110"
+                className="rounded loginLogo"
+                alt="Liquiz Logo"
+                />
+                <br></br>
+                <h1 className="welcomeText">Getting Started With Liquiz</h1>
+                <br></br>
+                <br></br>
+              <Form onSubmit={handleSubmit}>
+                  <Form.Group className="mb-3" value={user.username}
+                  onChange={handleChange}>
+                      <Form.Label className="labelLogin bsPrefix">Username</Form.Label>
+                      <Form.Control className="formLogin" name="username" type="text"  />
+                  </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicPassword" value={user.password}
-                onChange={handleChange}>
-                    <Form.Control name="password" type="password" placeholder="Masukkan Password" />
-                </Form.Group>
-                <div className='option'>Sudah punya akun? Masuk <a href='/login'>disini.</a></div>
-                <Button onClick={Register} variant="primary" type="submit">
-                    Daftar
-                </Button>
-            </Form>
-        </div>
+                  <Form.Group className="mb-3" controlId="formBasicPassword" value={user.password}
+                  onChange={handleChange}>
+                      <Form.Label className="labelLogin bsPrefix">Password</Form.Label>
+                      <Form.Control name="password" type="password" className="formLogin" />
+                  </Form.Group>
+                  <br></br>
+                  <div className='option'>Already have an account?<a href='/login'>Login here</a></div>
+                  <Button onClick={Register} variant="primary" type="submit" className="loginButton">
+                      REGISTER
+                  </Button>
+              </Form>
+          </div>
+          </Col>
+        </Row>
+      </Card>
+    </Container>
+
+
+
     )
 }
 
